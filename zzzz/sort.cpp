c735cdab95a7adaf9d4e02abc9b90f2815ddb869
GabrielPaz00 <gabriel.paz.190@gmail.com>
2023-11-09 03:48:36 -0600

    add doc

--- zzzz/sort.cpp
+++ zzzz/sort.cpp
@@ -2,6 +2,11 @@
 #include <vector>
 
 using namespace std;
+/**
+ * Imprime un arreglo de enteros en la consola.
+ * @param array El arreglo de enteros a imprimir.
+ * @param length La longitud del arreglo.
+ */
 void printArray(int* array, int length) {
     cout << "[";
     for (int i = 0; i < length; i++) {
@@ -12,6 +17,12 @@ void printArray(int* array, int length) {
     }
     cout << "]" << endl;
 }
+/**
+ * @brief Ordena un arreglo de enteros utilizando el algoritmo Bubble Sort.
+ *
+ * @param array Arreglo de enteros a ordenar.
+ * @param length Longitud del arreglo.
+ */
 void bubbleSort(int* array, int length) {
     int i, aux;
     int j = length - 1;
@@ -26,6 +37,12 @@ void bubbleSort(int* array, int length) {
         }
     }
 }
+/**
+ * @brief Ordena un arreglo de enteros utilizando el algoritmo Shaker Sort.
+ *
+ * @param array Arreglo de enteros a ordenar.
+ * @param length Longitud del arreglo.
+ */
 void shakerSort(int* array, int length) {
     int i;
     int j = length - 1;
@@ -50,6 +67,12 @@ void shakerSort(int* array, int length) {
         }
     }
 }
+/**
+ * Ordena un arreglo de enteros utilizando el algoritmo de selección.
+ *
+ * @param array El arreglo a ordenar.
+ * @param length La longitud del arreglo.
+ */
 void selectionSort(int* array, int length) {
     int i;
     int n = length - 1;
@@ -68,6 +91,11 @@ void selectionSort(int* array, int length) {
         array[min_index] = aux;
     }
 }
+/**
+ * Ordena un arreglo de enteros utilizando el algoritmo de ordenamiento por inserción.
+ * @param array El arreglo a ordenar.
+ * @param length La longitud del arreglo.
+ */
 void insertionSort(int* array, int length) {
     int i, j, key;
     for (i = 1; i < length; i++) {
@@ -80,12 +108,57 @@ void insertionSort(int* array, int length) {
         array[j + 1] = key;
     }
 }
-void binaryInsertionSort() {
+/**
+ * @param array Arreglo ordenado en el que se realizará la búsqueda.
+ * @param low Índice inferior del arreglo.
+ * @param high Índice superior del arreglo.
+ */
+int binarySearch(int* array, int low, int high, int key) {
 
+    if (high <= low) {
+        return (key > array[low]) ? (low + 1) : low;
+    }
+    int mid = (low + high) / 2;
+    if (key == array[mid]) {
+        return mid;
+    }
+    if (key > array[mid]) {
+        return binarySearch(array, mid + 1, high, key);
+    }
+    else {
+        return binarySearch(array, low, mid - 1, key);
+    }
+}
+/**
+ * Ordena un arreglo de enteros utilizando el algoritmo de ordenamiento por inserción binaria.
+ * @param array El arreglo a ordenar.
+ * @param length La longitud del arreglo.
+ */
+void binaryInsertionSort(int* array, int length) {
+    int i, j, key, location;
+    for (i = 1; i < length; i++) {
+        key = array[i];
+        location = binarySearch(array, 0, i - 1, key);
+        for (j = i - 1; j >= location; j--) {
+            array[j + 1] = array[j];
+        }
+        array[j + 1] = key;
+        cout << endl << "corrida " << i << " : ";
+        printArray(array, length);
+        cout << endl;
+    }
 }
 void shellSort() {
 
 }
+/**
+ * @brief Particiona un arreglo alrededor de un elemento pivote.
+ *
+ * @param array Puntero al arreglo que se va a particionar.
+ * @param low Índice del primer elemento de la partición.
+ * @param high Índice del último elemento de la partición.
+ * @return Índice del elemento pivote después de la partición.
+ */
 int partition(int* array, int low, int high) {
     int pivot = array[high];
     int i = low - 1;
@@ -98,6 +171,12 @@ int partition(int* array, int low, int high) {
     swap(array[i + 1], array[high]);
     return i + 1;
 }
+/**
+ * Ordena un arreglo de enteros utilizando el algoritmo QuickSort.
+ * @param array El arreglo a ordenar.
+ * @param low El índice más bajo del arreglo a ordenar.
+ * @param high El índice más alto del arreglo a ordenar.
+ */
 void quickSort(int* array, int low, int high) {
     if (low < high) {
         int pivot = partition(array, low, high);
@@ -108,6 +187,13 @@ void quickSort(int* array, int low, int high) {
 void heapSort() {
 
 }
+/**
+ * @brief Busca un valor en un arreglo y muestra las posiciones donde se encuentra.
+ *
+ * @param value El valor a buscar.
+ * @param array El arreglo donde se buscará el valor.
+ * @param length La longitud del arreglo.
+ */
 void findValue(int value, int* array, int length) {
     bool found = false;
     vector<int> positions;
@@ -137,6 +223,21 @@ void findValue(int value, int* array, int length) {
     }
     return;
 }
+/**
+ * Ordena un arreglo de enteros utilizando uno de los siguientes algoritmos de ordenamiento:
+ * 1. Bubble sort
+ * 2. Shaker sort
+ * 3. Selection sort
+ * 4. Insertion sort
+ * 5. Binary insertion sort
+ * 6. Shell sort
+ * 7. Quick sort
+ * 8. Heap sort
+ *
+ * @param array El arreglo de enteros a ordenar
+ * @param length La longitud del arreglo
+ * @param op El número que representa el algoritmo de ordenamiento a utilizar
+ */
 void sortArray(int* array, int length, int op) {
     string message;
     switch (op)
@@ -158,13 +259,20 @@ void sortArray(int* array, int length, int op) {
         message = "Insertion sort";
         break;
     case 5:
+        binaryInsertionSort(array, length);
+        message = "Binary insertion sort";
         break;
     case 6:
+        shellSort();
+        message = "shell sort";
         break;
     case 7:
         quickSort(array, 0, length - 1);
         message = "Quick sort";
         break;
+    case 8:
+        heapSort();
+        message = "Heap sort";
     default:
         break;
     }
@@ -173,6 +281,11 @@ void sortArray(int* array, int length, int op) {
     printArray(array, length);
     cout << endl;
 }
+/**
+ * Muestra un menú para que el usuario ingrese el tamaño de un vector y elija un método de ordenamiento.
+ * Luego, muestra el vector desordenado, lo ordena según el método elegido por el usuario y busca un valor dentro del vector.
+ * Si el usuario desea seguir ordenando, se muestra el menú nuevamente.
+ */
 void showMenu() {
     int length, value, exit, op;
     cout << "\t Ingresa el tamaño del vector: ";
